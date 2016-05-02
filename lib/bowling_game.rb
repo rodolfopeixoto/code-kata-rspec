81283df680d7d8c91e0e1cc3245911dd89b9e9d6
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-05-02 21:22:49 +0000

    spare example green

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,6 +1,26 @@
 class BowlingGame
-    attr_reader :score
+    
     def pins(rolls)
-        @score = rolls.inject(:+)
+        @rolls = rolls
+    end
+    
+    def score
+        frame = 0
+        score = 0
+        first_in_frame = 0
+        
+        while frame < 10
+        
+          if @rolls[first_in_frame] + @rolls[first_in_frame + 1] == 10 #spare
+           score += 10 + @rolls[first_in_frame + 2]
+          else
+           score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
+          end
+        
+           frame += 1
+           first_in_frame += 2
+        end
+        
+        score
     end
 end
