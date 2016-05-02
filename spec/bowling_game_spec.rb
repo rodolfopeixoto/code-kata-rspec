8a2c529f489295a09ba1bbbad7b3d2f2d8af12ad
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-05-02 21:01:29 +0000

    Refactor test with let

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,14 +1,15 @@
 require_relative '../lib/bowling_game'
 
 describe BowlingGame do
-    it 'scores a gutter game' do
-      game = BowlingGame.new
+  
+  let(:game) { BowlingGame.new }
+  
+    it 'scores a gutter game' do 
       game.pins([0] * 20)
       expect(game.score).to eq(0)
     end
     
-    it 'scores a game 1s' do
-      game = BowlingGame.new
+    it 'scores a game 1s' do 
       game.pins([1] * 20)
       expect(game.score).to eq(20)
     end
